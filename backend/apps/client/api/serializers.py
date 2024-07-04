126d5f7c675806a8df51dedd2201dbef54346af1
JavierBrizuela <jbrizuela1976@gmail.com>
2024-07-03 23:06:03 -0300

    add: destroy to viewset client

--- backend/apps/client/api/serializers.py
+++ backend/apps/client/api/serializers.py
@@ -5,7 +5,7 @@ from ..models import Client, IndividualClient, CompanyClient
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = ['is_company', 'email', 'phone', 'zip_code']
+        fields = ['id', 'is_company', 'email', 'phone', 'zip_code']
 
 class IndividualClientSerializer(serializers.ModelSerializer):
     class Meta:
@@ -24,6 +24,7 @@ class CompleteClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = [
+            'id',
             'is_company', 
             'email', 
             'phone', 
@@ -37,7 +38,6 @@ class CompleteClientSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Company data is required for company clients")
         if not data['is_company'] and 'individual' not in data:
             raise serializers.ValidationError("Individual data is required for company clients")
-        print(data)
         return data
         
     def create(self, validate_data):
@@ -46,18 +46,29 @@ class CompleteClientSerializer(serializers.ModelSerializer):
         individual_data = validate_data.pop('individual', None)
         
         client = Client.objects.create(**validate_data)
-        
+        response = ClientSerializer(client).data
         if client.is_company:
-            CompanyClient.objects.create(client_id=client, **company_data)
+            company = CompanyClient.objects.create(client_id=client, **company_data)
+            response['company'] = CompanyClientSerializer(company).data
+            return response
         else:
-            IndividualClient.objects.create(client_id=client, **individual_data)
-        
-        return client
+            individual = IndividualClient.objects.create(client_id=client, **individual_data)
+            response['individual'] = IndividualClientSerializer(individual).data
+            return response
     
-    """ def to_representation(self, instance):
+    """def to_representation(self, instance):
+        if isinstance(instance, dict):
+            # Si es un diccionario (resultado de create), lo devolvemos directamente
+            return instance
+        # Si es una instancia de Client (para GET requests), usamos la representación normal
         representation = super().to_representation(instance)
-        client_representation = ClientSerializer(instance).data
-        representation['client'] = client_representation
+        if instance.is_company:
+            representation['company'] = CompanyClientSerializer(instance.companyclient).data
+        else:
+            representation['individual'] = IndividualClientSerializer(instance.individualclient).data
+        return representation"""
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
 
         if instance.is_company:
             company = CompanyClient.objects.get(client_id=instance)
@@ -68,4 +79,4 @@ class CompleteClientSerializer(serializers.ModelSerializer):
             individual_representation = IndividualClientSerializer(individual).data
             representation['individual'] = individual_representation
 
-        return representation """
+        return representation 
--- backend/apps/client/api/viewsets.py
+++ backend/apps/client/api/viewsets.py
@@ -1,8 +1,23 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import CompleteClientSerializer
-from ..models import Client
+from ..models import Client, CompanyClient, IndividualClient
 
 class ClientViewSet(ModelViewSet):
     serializer_class = CompleteClientSerializer
     queryset = Client.active_objects.all()
+    
+    def destroy(self, request, *args, **kwargs):
+        
+        instance = self.get_object()
+        print(instance)
+        if instance.is_company:
+            company = CompanyClient.objects.filter(client_id=instance).first()
+            company.soft_delete()
+        else:
+            individual = IndividualClient.objects.filter(client_id=instance).first()
+            individual.soft_delete()
+        instance.soft_delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
